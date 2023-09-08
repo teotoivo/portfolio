6323b7e7a878294a86d67b3b741620730a0f1cff
teotoivo <teo.maximilien@gmail.com>
2023-09-08 13:01:04 +0300

    fuckshit

--- components/LoginButton.tsx
+++ components/LoginButton.tsx
@@ -9,11 +9,15 @@ import {
 } from "@supabase/auth-helpers-nextjs";
 import { Database } from "@/types/supabase";
 import UserMenu from "./UserMenu";
+import Login from "@/components/Login";
+import SignUp from "@/components/SignUp";
 
 export default function LoginButton({ session }: { session: Session | null }) {
   const path = usePathname();
   const supabase = createClientComponentClient<Database>();
   const routePath = path.split("/")[1];
+  const [showLogin, setShowLogin] = React.useState(false);
+  const [showSignup, setShowSignup] = React.useState(false);
 
   return (
     <>
@@ -22,17 +26,33 @@ export default function LoginButton({ session }: { session: Session | null }) {
           return session?.user ? (
             <UserMenu />
           ) : (
-            <Link
+            /*<Link
               href="./user/login"
               className="bg-btn-background rounded-3xl border-2 p-2 px-5 hover:border-btn-border-hover hover:scale-105 ease-in-out duration-200 border-btn-border shadow-btn-main shadow-white"
             >
               <p>Login</p>
-            </Link>
+            </Link>*/
+            <button
+              className="rounded-3xl border-2 border-btn-border bg-btn-background p-2 px-5 shadow-btn-main shadow-white duration-200 ease-in-out hover:scale-105 hover:border-btn-border-hover"
+              onClick={() => setShowLogin(!showLogin)}
+            >
+              <p>Login</p>
+            </button>
           );
         } else {
           return <></>;
         }
       })()}
+      {showLogin ? (
+        <Login setShowLogin={setShowLogin} setShowSignup={setShowSignup} />
+      ) : (
+        <></>
+      )}
+      {showSignup ? (
+        <SignUp setShowLogin={setShowLogin} setShowSignup={setShowSignup} />
+      ) : (
+        <></>
+      )}
     </>
   );
 }
---
+++ components/SignUp.tsx
@@ -0,0 +1,186 @@
+"use client";
+import React, { useRef, useState } from "react";
+
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+import type { Database } from "@/types/supabase";
+import Link from "next/link";
+import { AuthResponse } from "@supabase/supabase-js";
+import ErrorComponent from "@/components/ErrorComponent";
+
+import OutsideAlerter from "./OutsideAlerter";
+
+export default function SignUp({
+  setShowLogin,
+  setShowSignup,
+}: {
+  setShowLogin: React.Dispatch<React.SetStateAction<boolean>>;
+  setShowSignup: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+  const router = useRouter();
+  const supabase = createClientComponentClient<Database>();
+  const formRef = useRef<HTMLFormElement>(null);
+  const [isPassValid, setIsPassValid] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const displayError = (message: string) => {
+    setError(message);
+  };
+
+  supabase.auth.onAuthStateChange((event, session) => {
+    if (event == "SIGNED_IN") {
+      router.push("/user");
+    }
+  });
+
+  const handleSignUp = async (
+    email: string,
+    password: string,
+    first_name: string,
+    last_name: string,
+    age: string,
+  ) => {
+    const {
+      data: { user, session },
+      error,
+    }: AuthResponse = await supabase.auth.signUp({
+      email: email,
+      password: password,
+      options: {
+        emailRedirectTo: `${location.origin}/user/auth/callback`,
+        data: {
+          first_name: first_name,
+          last_name: last_name,
+          age: age,
+        },
+      },
+    });
+
+    if (error) displayError(error.message);
+
+    router.push(`/user/verifyemail?email=${email}`);
+  };
+  return (
+    <>
+      <div className="fixed left-0 top-0 flex h-screen w-screen items-center justify-center bg-background-with-opacity">
+        <div className=" flex h-fit w-fit flex-col items-center justify-center rounded-md bg-background-with-opacity p-10">
+          <p>Sign Up</p>
+          <form
+            className="flex flex-col items-center justify-center gap-4"
+            action=""
+            ref={formRef}
+            onSubmit={(e) => {
+              e.preventDefault();
+              const email = formRef.current?.elements.namedItem(
+                "email",
+              ) as HTMLInputElement;
+              const password = formRef.current?.elements.namedItem(
+                "password",
+              ) as HTMLInputElement;
+              const confirm_password = formRef.current?.elements.namedItem(
+                "confirm_password",
+              ) as HTMLInputElement;
+              const first_name = formRef.current?.elements.namedItem(
+                "first_name",
+              ) as HTMLInputElement;
+              const last_name = formRef.current?.elements.namedItem(
+                "last_name",
+              ) as HTMLInputElement;
+              const age = formRef.current?.elements.namedItem(
+                "age",
+              ) as HTMLInputElement;
+              if (password.value === confirm_password.value) {
+                handleSignUp(
+                  email.value,
+                  password.value,
+                  first_name.value,
+                  last_name.value,
+                  age.value,
+                );
+              } else {
+                displayError("Passwords do not match");
+              }
+            }}
+          >
+            <input
+              type="text"
+              name="email"
+              id="email"
+              className="rounded-md border-2 border-btn-border bg-background p-1"
+              required
+              placeholder="Email"
+            />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              className="rounded-md border-2 border-btn-border bg-background p-1"
+              required
+              placeholder="Password"
+            />
+            <input
+              type="password"
+              name="confirm_password"
+              id="confirm_password"
+              className={`rounded-md border-2 border-btn-border bg-background p-1 focus:outline-none ${
+                isPassValid ? "border-green-500" : "border-red-500"
+              }`}
+              required
+              placeholder="Confirm Password"
+              onChange={(e) => {
+                const password = formRef.current?.elements.namedItem(
+                  "password",
+                ) as HTMLInputElement;
+                if (e.target.value === password.value) {
+                  setIsPassValid(true);
+                } else {
+                  setIsPassValid(false);
+                }
+              }}
+            />
+            <input
+              type="text"
+              name="first_name"
+              id="first_name"
+              className="rounded-md border-2 border-btn-border bg-background p-1"
+              required
+              placeholder="First Name"
+            />
+            <input
+              type="text"
+              name="last_name"
+              id="last_name"
+              className="rounded-md border-2 border-btn-border bg-background p-1"
+              required
+              placeholder="Last Name"
+            />
+            <input
+              type="text"
+              name="age"
+              id="age"
+              className="rounded-md border-2 border-btn-border bg-background p-1"
+              required
+              placeholder="Age"
+            />
+            <button
+              type="submit"
+              className="rounded-lg border-2 border-btn-border bg-btn-background p-2 transition-all hover:scale-105 hover:border-btn-border-hover"
+            >
+              Sign up
+            </button>
+            <ErrorComponent error={error} />
+            <button
+              className="text-md underline"
+              onClick={() => {
+                setShowSignup(false);
+                setShowLogin(true);
+              }}
+            >
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
